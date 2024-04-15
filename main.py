c139f4fdd966cf8b58fa5d726886b03ec1f382c9
aldredb <aldred.halim@mongodb.com>
2024-04-15 15:50:52 +0800

    As per request

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ with col4:
     bucket = st.text_input("GCS Bucket :bucket:", help="Include a trailing slash", placeholder="bucket-name/")
 
 with col5:
-    access_token = st.text_input("GCP Access Token :key:", type="password", help="Optional. If GCS bucket is not public, retrieve token by issuing 'gcloud auth login' and 'gcloud auth print-access-token'")
+    access_token = st.text_input("GCP Access Token :key:", type="password", placeholder="Optional - for private buckets", help="If GCS bucket is private, retrieve token by issuing 'gcloud auth login' and 'gcloud auth print-access-token'")
 
 all_fields_filled = (
     openai_api_key != '' and
